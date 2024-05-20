67a686952caebe28e38549c16cbda21331d6c975
Melanie Wilson <melaniemwilson1@gmail.com>
2024-05-20 17:15:13 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,42 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.order_by('created_at').all()]
+        return messages, 200
 
-@app.route('/messages/<int:id>')
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        return new_message.to_dict(), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method == 'PATCH':
+
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+
+        db.session.add(message)
+        db.session.commit()
+
+        return message.to_dict(), 200
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
 
 if __name__ == '__main__':
     app.run(port=5555)
